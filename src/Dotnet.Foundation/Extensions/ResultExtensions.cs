5ea40036c43c049fec76eb4bcb941cd6b5d23585
Cătălin-Ionuț Cucu <catalinionut.cucu@gmail.com>
2026-03-14 16:19:28 +0200

    feat: add result object and extensions for result object

---
+++ src/Dotnet.Foundation/Extensions/ResultExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Dotnet.Foundation.Models;
+
+namespace Dotnet.Foundation.Extensions;
+
+public static class ResultExtensions
+{
+    /// <summary>
+    /// Matches a <see cref = "Result{TValue,TError}" /> instance to a value of type <typeparamref name = "TMap" /> based on the state.
+    /// </summary>
+    /// <returns>A value of type <typeparamref name = "TMap" />.</returns>
+    public static TMap Match<TMap, TValue, TError>(this Result<TValue, TError> result, Func<TValue, TMap> success, Func<TError, TMap> failure)
+    {
+        ArgumentNullException.ThrowIfNull(result);
+        ArgumentNullException.ThrowIfNull(success);
+        ArgumentNullException.ThrowIfNull(failure);
+
+        return result.IsSuccess ? success(result.Value) : failure(result.Error);
+    }
+
+    /// <summary>
+    /// Matches a <see cref = "Result{TError}" /> instance to a value of type <typeparamref name = "TMap" /> based on the state.
+    /// </summary>
+    /// <returns>A value of type <typeparamref name = "TMap" />.</returns>
+    public static TMap Match<TMap, TError>(this Result<TError> result, Func<TMap> success, Func<TError, TMap> failure)
+    {
+        ArgumentNullException.ThrowIfNull(result);
+        ArgumentNullException.ThrowIfNull(success);
+        ArgumentNullException.ThrowIfNull(failure);
+
+        return result.IsSuccess ? success() : failure(result.Error);
+    }
+}
---
+++ src/Dotnet.Foundation/Models/Result.cs
@@ -0,0 +1,89 @@
+﻿namespace Dotnet.Foundation.Models;
+
+public sealed class Result<TValue, TError>
+{
+    public ResultState State { get; }
+
+    public TValue Value => State == ResultState.Success ? field : throw new InvalidOperationException($"Property '{nameof(Value)}' cannot be accessed on failure result.");
+
+    public TError Error => State == ResultState.Failure ? field : throw new InvalidOperationException($"Property '{nameof(Error)}' cannot be accessed on success result.");
+
+    public bool IsSuccess => State is ResultState.Success;
+
+    public bool IsFailure => State is ResultState.Failure;
+
+    private Result(TValue value)
+    {
+        State = ResultState.Success;
+        Value = value;
+    }
+
+    private Result(TError error)
+    {
+        State = ResultState.Failure;
+        Error = error;
+    }
+
+    public static Result<TValue, TError> Success(TValue value)
+    {
+        return new(value);
+    }
+
+    public static Result<TValue, TError> Failure(TError error)
+    {
+        return new(error);
+    }
+
+    public static implicit operator Result<TValue, TError>(TValue value)
+    {
+        return Success(value);
+    }
+
+    public static implicit operator Result<TValue, TError>(TError error)
+    {
+        return Failure(error);
+    }
+}
+
+public sealed class Result<TError>
+{
+    public ResultState State { get; }
+
+    public TError Error => State == ResultState.Failure ? field : throw new InvalidOperationException($"Property '{nameof(Error)}' cannot be accessed on success result.");
+
+    public bool IsSuccess => State is ResultState.Success;
+
+    public bool IsFailure => State is ResultState.Failure;
+
+    private Result()
+    {
+        State = ResultState.Success;
+    }
+
+    private Result(TError error)
+    {
+        State = ResultState.Failure;
+        Error = error;
+    }
+
+    public static Result<TError> Success()
+    {
+        return new();
+    }
+
+    public static Result<TError> Failure(TError error)
+    {
+        return new(error);
+    }
+
+    public static implicit operator Result<TError>(TError error)
+    {
+        return Failure(error);
+    }
+}
+
+public enum ResultState
+{
+    Success,
+    Failure
+}
