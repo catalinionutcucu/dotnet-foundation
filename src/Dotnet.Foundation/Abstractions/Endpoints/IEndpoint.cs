0c95572db6c3d292a024016ae8b83adbe6731076
Cătălin-Ionuț Cucu <catalinionut.cucu@gmail.com>
2026-03-15 15:24:33 +0200

    feat: add endpoint abstractions and extensions for endpoint registration

---
+++ src/Dotnet.Foundation/Abstractions/Endpoints/IEndpoint.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace Dotnet.Foundation.Abstractions.Endpoints;
+
+public interface IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder endpointRouteBuilder);
+}
