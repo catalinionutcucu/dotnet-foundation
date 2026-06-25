32cf10a4b8a0e488505555c83d2691e45e8e429e
Cătălin-Ionuț Cucu <catalinionut.cucu@gmail.com>
2026-06-25 07:00:33 +0300

    feat: add request error object with extension methods for handling http responses

---
+++ src/Dotnet.Foundation/Models/RequestError.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Immutable;
+
+namespace Dotnet.Foundation.Models;
+
+public sealed class RequestError
+{
+    public RequestErrorType Type { get; }
+
+    public string Code { get; }
+
+    public ImmutableArray<string> Issues { get; }
+
+    private RequestError(RequestErrorType type, string code, params IEnumerable<string> issues)
+    {
+        ArgumentNullException.ThrowIfNull(code);
+        ArgumentNullException.ThrowIfNull(issues);
+
+        Type = type;
+        Code = code;
+        Issues = [ ..issues ];
+    }
+
+    /// <summary>
+    /// Creates a <see cref = "RequestError" /> instance representing a request invalid error.
+    /// </summary>
+    /// <returns>A <see cref = "RequestError" /> instance representing a request invalid error.</returns>
+    public static RequestError RequestInvalid(string code, params IEnumerable<string> issues)
+    {
+        return new(RequestErrorType.RequestInvalid, code, issues);
+    }
+
+    /// <summary>
+    /// Creates a <see cref = "RequestError" /> instance representing a request not allowed error.
+    /// </summary>
+    /// <returns>A <see cref = "RequestError" /> instance representing a request not allowed error.</returns>
+    public static RequestError RequestNotAllowed(string code, params IEnumerable<string> issues)
+    {
+        return new(RequestErrorType.RequestNotAllowed, code, issues);
+    }
+
+    /// <summary>
+    /// Creates a <see cref = "RequestError" /> instance representing a resource not found error.
+    /// </summary>
+    /// <returns>A <see cref = "RequestError" /> instance representing a resource not found error.</returns>
+    public static RequestError ResourceNotFound(string code, params IEnumerable<string> issues)
+    {
+        return new(RequestErrorType.ResourceNotFound, code, issues);
+    }
+
+    /// <summary>
+    /// Creates a <see cref = "RequestError" /> instance representing a resource conflict error.
+    /// </summary>
+    /// <returns>A <see cref = "RequestError" /> instance representing a resource conflict error.</returns>
+    public static RequestError ResourceConflict(string code, params IEnumerable<string> issues)
+    {
+        return new(RequestErrorType.ResourceConflict, code, issues);
+    }
+}
+
+public enum RequestErrorType
+{
+    RequestInvalid,
+    RequestNotAllowed,
+    ResourceNotFound,
+    ResourceConflict
+}
